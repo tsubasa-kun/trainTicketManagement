1c8de65e3d5b9bcf15fb1bf0e319da33b793fb6d
Cookie <tsubasa-kun@foxmail.com>
2017-05-06 00:05:42 +0800

    完善登录UI

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,10 +1,63 @@
 <template>
-    <div></div>
+    <div class="content">
+        <div>
+            <img src="../../assets/title_bg.jpg">
+        </div>
+        <div class="login-info-div">
+            <mt-field label="账　号：" placeholder="请输入账号" v-model="username"></mt-field>
+            <mt-field label="密　码：" placeholder="请输入密码" type="password" v-model="password"></mt-field>
+            <mt-button type="primary" class="login-btn" @click="doLogin">登录</mt-button>
+        </div>
+    </div>
 </template>
 
 <style scoped>
+    .content {
+        text-align: center;
+        padding-top: 50PX;
+    }
+
+    .content img {
+        width: 25%;
+        height: 200PX;
+    }
+
+    .login-info-div {
+        width: 25%;
+        margin: 0 auto;
+    }
+
+    .login-btn {
+        width: 50%;
+        margin-top: 20PX;
+    }
 </style>
 
 <script type="text/javascript">
-    export default {}
+    import {Field, Button} from 'mint-ui';
+    import Utils from '../../utils';
+
+    export default {
+        data() {
+            return {
+                username: '',
+                password: ''
+            }
+        },
+        components: {
+            [Field.name]: Field,
+            [Button.name]: Button
+        },
+        methods: {
+            doLogin: function () {
+                if (this.username.length === 0) {
+                    Utils.methods.showToast('请输入账号');
+                } else if (this.password.length === 0) {
+                    Utils.methods.showToast('请输入密码');
+                } else {
+
+                }
+            }
+        }
+    }
 </script>
--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,22 @@
 /**
  * Created by jk on 2017/4/21 0021.
  */
-import {Toast} from 'mint-ui';
+import {Toast, MessageBox} from 'mint-ui';
 export default {
     components: {
-        [Toast.name]: Toast
+        [Toast.name]: Toast,
+        [MessageBox.name]: MessageBox
     },
     methods: {
-        showTip: function (msg) {
+        showToast: function (msg) {
             Toast({
                 message: msg,
-                position: 'bottom',
+                position: 'middle',
                 duration: 2000
             });
+        },
+        showMessage: function (msg) {
+            MessageBox.alert(msg, '提示');
         }
     }
 }
